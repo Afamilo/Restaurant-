c729d706c3c36c01ccfac95da334b291ea2056dc
Afamilo <nichomartins@gmail.com>
2024-01-14 22:56:27 -0600

    creating element tags in the home page

---
+++ src/modules/home.js
@@ -0,0 +1,20 @@
+function createAboutSection() {
+    const about = document.createElement('section');
+    about.classList.add('section');
+
+    const title = document.createElement('h2');
+    title.classList.add('section-title');
+    title.textContent = 'About';
+
+    about.appendChild(title);
+
+    const para = document.createElement('p');
+    para.classList.add('section-descrip');
+    para.textContent =
+    'Kitchen Repblic has the best food from the all whiled world';
+
+    about.appendChild(para);
+    return about;
+}
+
+export default loadHome;
